18720ba84fc44c704ff72be2afa13adce4f9a46a
Feilieruhesoj <152266727+Feilieruhesoj@users.noreply.github.com>
2024-09-27 22:11:44 +0800

    Add recycler view for list about me

--- app/src/main/java/com/example/cc17_3f_joselhl_gitrampup/MainActivity.kt
+++ app/src/main/java/com/example/cc17_3f_joselhl_gitrampup/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cc17_3f_joselhl_gitrampup.adapter.AboutMeAdapter
+import com.example.cc17_3f_joselhl_gitrampup.model.AboutMe
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val rvAboutMe: RecyclerView = findViewById(R.id.rvAboutMe)
+        rvAboutMe.layoutManager = LinearLayoutManager(this)
+
+
+        val aboutMe1 = AboutMe(R.mipmap.ic_launcher,
+            "Jose Leif Hurei L.",
+            "short desc",
+            { /*TODO*/})
+
+        val aboutMe2 = AboutMe(R.mipmap.ic_launcher,
+            "Jane Doe",
+            "ZZZ",
+            {/*TODO*/})
+
+        val arrlist = listOf(aboutMe1, aboutMe2)
+
+
+
+
+        rvAboutMe.adapter = AboutMeAdapter(arrlist)
+
     }
 }
---
+++ app/src/main/java/com/example/cc17_3f_joselhl_gitrampup/model/AboutMe.kt
@@ -0,0 +1,6 @@
+package com.example.cc17_3f_joselhl_gitrampup.model
+
+import android.view.View.OnClickListener
+
+class AboutMe(val imageResId: Int, val name: String, val desc: String, val onClickListener: OnClickListener){
+}
